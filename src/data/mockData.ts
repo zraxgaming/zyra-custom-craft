0e1435f1811a4416aa979439c3db757f8e201482
zraxgaming <159037988+zraxgaming@users.noreply.github.com>
2025-05-12 22:13:54 +0400

    Update mockData.ts

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -1,5 +1,4 @@
-import { supabase } from 'src/integrations/supabase/client';
-
+import { supabase } from '../../integrations/supabase/client';
 export const fetchProducts = async () => {
   try {
     const { data, error } = await supabase
